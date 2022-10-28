c01f91f215273c8605e7fc0ed3f6ce44493dea90
Simon <simon.schleich@univie.ac.at>
2022-10-28 09:23:18 +0200

    Structure

--- main.py
+++ main.py
@@ -3,14 +3,19 @@ import matplotlib.pyplot as plt
 import util as u
 
 # GLOBALS
-INPUT_DATA = "JWST_Cycle1_Target.csv"
-#INPUT_DATA = "test_data.dat"
+INPUT_DATA = "data/JWST_Cycle1_Target.csv"
+# INPUT_DATA = "test_data.dat"
 
 
 def main():
     target_list = u.dict_from_tar_list(INPUT_DATA)
     target_keys = list(target_list.keys())
-    print(f"Available keys are {target_keys}\n")
+
+    # Sort for Transit Observations. Do this first, as making the
+    # dictionary unique might remove transit observations and leave e.g.
+    # eclipse observations of the same planet
+    ind_transit = np.where(target_list["Type"] == "Transit")
+    u.red_total_dict(target_list, ind_transit)
 
     # Make dictionary with unique entries (planets)
     u.make_dict_unique(target_list)
@@ -19,10 +24,15 @@ def main():
     u.check_nans(target_list)
 
     # Sort for super-Earths and mini-Neptunes
-    new_indices1 = np.where(target_list["Radius [RE]"] <= 4.)[0]
-    u.red_total_dict(target_list, new_indices1)
-    new_indices2 = np.where(target_list["Radius [RE]"] > 0.)[0]
-    u.red_total_dict(target_list, new_indices2)
+    ind_uppermass = np.where(target_list["Radius [RE]"] <= 4.)[0]
+    u.red_total_dict(target_list, ind_uppermass)
+
+    ind_lowermass = np.where(target_list["Radius [RE]"] > 0.)[0]
+    u.red_total_dict(target_list, ind_lowermass)
+
+    # Print out valid target names
+    print(target_list["Target Name"])
+    print(target_list["EAP [mon]"])
 
     # Plotting
     u.rc_setup()
@@ -32,18 +42,25 @@ def main():
     ax.scatter(target_list["SMA [au]"], target_list["Teff [K]"])
 
     for i in range(len(target_list["Target Name"])):
-        ax.annotate(target_list["Target Name"][i], (target_list["SMA [au]"][i], target_list["Teff [K]"][i]))
+        x, y = (target_list["SMA [au]"][i], target_list["Teff [K]"][i])
+        ax.annotate(target_list["Target Name"][i], (x, y))
 
-    temp = np.linspace(2600, 7200, 5000)
-    hz_bounds = u.plotable_hz_bounds(temp = temp)
-    ax.fill_betweenx(temp, x1=hz_bounds["oi"], x2=hz_bounds["oo"], color="tab:green", label="OHz (Kopparapu et al. 2013)")
-    ax.fill_betweenx(temp, x1=hz_bounds["ci"], x2=hz_bounds["co"], color="tab:orange", label="CHz (Kopparapu et al. 2013)")
+    temperature = np.linspace(2600, 7200, 5000)
+    hz_bounds = u.plotable_hz_bounds(temp=temperature)
+    # Optimistic boundaries from Kopparapu et al. 2013 (for 1Me)
+    ax.fill_betweenx(temperature, x1=hz_bounds["oi"], x2=hz_bounds["oo"],
+                     color="tab:green", label="OHz (Kopparapu et al. 2013)")
+    # Conservative boundaries from (see above)
+    ax.fill_betweenx(temperature, x1=hz_bounds["ci"], x2=hz_bounds["co"],
+                     color="tab:orange", label="CHz (Kopparapu et al. 2013)")
 
     ax.set(xscale="log", xlabel="a [au]", ylabel="T$_{eff}$ (Host) [K]")
 
+    # Plot clean-up
     plt.legend()
     plt.tight_layout()
     plt.savefig("target_list.png", dpi=300)
+
 
 if __name__ == "__main__":
     main()
--- util.py
+++ util.py
@@ -4,23 +4,23 @@ import matplotlib as mpl
 
 
 def rc_setup():
-	"""Generalized plot attributes"""
-	mpl.rcParams["xtick.direction"] = "in"
-	mpl.rcParams["xtick.labelsize"] = "large"
-	mpl.rcParams["xtick.major.width"] = 1.5
-	mpl.rcParams["xtick.minor.width"] = 1.5
-	mpl.rcParams["xtick.minor.visible"] = "True"
-	mpl.rcParams["xtick.top"] = "True"
-	
-	mpl.rcParams["ytick.direction"] = "in"
-	mpl.rcParams["ytick.labelsize"] = "large"
-	mpl.rcParams["ytick.major.width"] = 1.5
-	mpl.rcParams["ytick.minor.width"] = 1.5
-	mpl.rcParams["ytick.minor.visible"] = "True"
-	mpl.rcParams["ytick.right"] = "True"
-	
-	mpl.rcParams["axes.linewidth"] = 1.5
-	mpl.rcParams["axes.labelsize"] = "large"
+    """Generalized plot attributes"""
+    mpl.rcParams["xtick.direction"] = "in"
+    mpl.rcParams["xtick.labelsize"] = "large"
+    mpl.rcParams["xtick.major.width"] = 1.5
+    mpl.rcParams["xtick.minor.width"] = 1.5
+    mpl.rcParams["xtick.minor.visible"] = "True"
+    mpl.rcParams["xtick.top"] = "True"
+
+    mpl.rcParams["ytick.direction"] = "in"
+    mpl.rcParams["ytick.labelsize"] = "large"
+    mpl.rcParams["ytick.major.width"] = 1.5
+    mpl.rcParams["ytick.minor.width"] = 1.5
+    mpl.rcParams["ytick.minor.visible"] = "True"
+    mpl.rcParams["ytick.right"] = "True"
+
+    mpl.rcParams["axes.linewidth"] = 1.5
+    mpl.rcParams["axes.labelsize"] = "large"
 
 
 def dict_from_tar_list(filename):
@@ -30,14 +30,14 @@ def dict_from_tar_list(filename):
 
     # Fill up empty array spots
     for i in range(len(full_array)):
-            full_array[i] = fill_arr(full_array[i], 0.)
-    
+        full_array[i] = fill_arr(full_array[i], 0.)
+
     # First array element is list of headers
     headers = full_array[0]
     values = np.transpose(full_array[1:])
 
     # Create dictionary
-    target_set = {headers[i]: values[i] for i in range(column_number-1)}
+    target_set = {headers[i]: values[i] for i in range(column_number)}
 
     # Make all numbers value arrays into float arrays
     for key in target_set.keys():
@@ -54,7 +54,7 @@ def fill_arr(str_array, filler):
     for i in range(len(str_array)):
         if str_array[i] == "":
             str_array[i] = filler
-    
+
     return str_array
 
 
@@ -83,79 +83,77 @@ def make_dict_unique(target_dict):
 
 def check_nans(target_dict):
     dict_keys = list(target_dict.keys())
-    
-    by_column = [list(np.where(target_dict[key] != 0.)[0]) for key in dict_keys]
+
+    by_column = [list(np.where(target_dict[key] != 0.)[0])
+                 for key in dict_keys]
     nan_ind = np.unique(np.array(sum(by_column, [])))
 
     # Reduce all dictionary entries
     for key in dict_keys:
         target_dict[key] = target_dict[key][nan_ind]
-    
+
     return target_dict
 
 
-
-def plotable_hz_bounds(temp = np.linspace(2600, 7200, 5000),
-                       lbol = np.linspace(0.01, 1, 5000)):
+def plotable_hz_bounds(temp=np.linspace(2600, 7200, 5000),
+                       lbol=np.linspace(0.01, 1, 5000)):
     bounds = ["oi", "ci", "co", "oo"]
-    hz_bounds = {key: habitable_zone_distance(temp, lbol, key) for key in bounds}
+    hz_bounds = {key: habitable_zone_distance(temp, lbol, key)
+                 for key in bounds}
 
     return hz_bounds
 
 
+def habitable_zone_distance(effect_temp, lum, est_ident):
+    """
+    HZ estimation from Kopparapu et al. (2013, 2014).
 
-def habitable_zone_distance(T_eff, L, est_ident):
-	"""
-	HZ estimation from Kopparapu et al. (2013, 2014).
-	
-	:param T_eff: NDARRAY,
-		Stellar temperature in Kelvin
-	:param L: NDARRAY,
-		Stellar bolometric luminosity in solar units
-	:param est_ident: STR,
-		Identifier for the estimation boundary
-	:return: NDARRAY,
-		HZ distance in AU
-	"""
-	# Determine valid indicators
-	valid_ind = ["oi", "ci", "co", "oo"]
-	est_indices = dict(zip(valid_ind, range(4)))
-	
-	# SANITY CHECK: indicator for estimate must exist
-	assert est_ident.lower() in ["oi", "ci", "co", "oo"], \
-		f"INDICATOR {est_ident} FOR ESTIMATION METHOD NOT RECOGNIZED!"
-	
-	# Parameter matrix organized by oi, ci, co, oo estimates
-	# PLEASE NOTE THE ERRATUM TO THE ORIGINAL KOPPARAPU (2013) PAPER!
-	param = np.array([
-		[1.4335e-4, 3.3954e-9, -7.6364e-12, -1.1950e-15],
-		[1.2456e-4, 1.4612e-8, -7.6345e-12, -1.7511e-15],
-		[5.9578e-5, 1.6707e-9, -3.0058e-12, -5.1925e-16],
-		[5.4471e-5, 1.5275e-9, -2.1709e-12, -3.8282e-16],
-	])
-	
-	# Set correct param-subindex according to est_ident
-	est_index = est_indices[est_ident]
-	
-	# Call the S_eff calculation function with correct parameters
-	S_eff = effective_flux(param[est_index], T_eff, est_index)
-	
-	# Calculate distance
-	distance = np.sqrt(L / S_eff)
-	
-	return distance
-	
-	
-def effective_flux(param_list, T_eff, estimation_index):
-	"""Intermediate step in HZ calculation"""
-	S_effsun = [1.7763, 1.0385, 0.3507, 0.3207]
-	
-	# Temperature array consisting of powers 1 to 4
-	temp = np.array(
-		[(T_eff - 5780) ** (i + 1) for i in range(4)]
-	)
-	
-	# Transpose the temperature-power array to have each row be a list
-	# of Temp ** 1 to Temp ** 4, and then calculate the dot-product
-	# with the parameter list vector
-	return S_effsun[estimation_index] + temp.T @ param_list
+    :param effect_temp: NDARRAY, Stellar temperature in Kelvin
+    :param lum: NDARRAY, Stellar bolometric luminosity in solar units
+    :param est_ident: STR, Identifier for the estimation boundary
+
+    :return: NDARRAY, HZ distance in AU
+    """
+
+    # Determine valid indicators
+    valid_ind = ["oi", "ci", "co", "oo"]
+    est_indices = dict(zip(valid_ind, range(4)))
+
+    # SANITY CHECK: indicator for estimate must exist
+    assert est_ident.lower() in ["oi", "ci", "co", "oo"], \
+        f"INDICATOR {est_ident} FOR ESTIMATION METHOD NOT RECOGNIZED!"
+
+    # Parameter matrix organized by oi, ci, co, oo estimates
+    # PLEASE NOTE THE ERRATUM TO THE ORIGINAL KOPPARAPU (2013) PAPER!
+    param = np.array([
+        [1.4335e-4, 3.3954e-9, -7.6364e-12, -1.1950e-15],
+        [1.2456e-4, 1.4612e-8, -7.6345e-12, -1.7511e-15],
+        [5.9578e-5, 1.6707e-9, -3.0058e-12, -5.1925e-16],
+        [5.4471e-5, 1.5275e-9, -2.1709e-12, -3.8282e-16],
+    ])
+
+    # Set correct param-subindex according to est_ident
+    est_index = est_indices[est_ident]
+
+    # Call the S_eff calculation function with correct parameters
+    s_eff = effective_flux(param[est_index], effect_temp, est_index)
+
+    # Calculate distance
+    distance = np.sqrt(lum / s_eff)
+
+    return distance
+
+
+def effective_flux(param_list, effect_temp, estimation_index):
+    """Intermediate step in HZ calculation"""
+    s_effsun = [1.7763, 1.0385, 0.3507, 0.3207]
+
+    # Temperature array consisting of powers 1 to 4
+    temp = np.array(
+        [(effect_temp - 5780) ** (i + 1) for i in range(4)]
+    )
+
+    # Transpose the temperature-power array to have each row be a list
+    # of Temp ** 1 to Temp ** 4, and then calculate the dot-product
+    # with the parameter list vector
+    return s_effsun[estimation_index] + temp.T @ param_list
