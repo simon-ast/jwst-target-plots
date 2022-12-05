8d171e553a97f54991d8f7c3a2f2b836e755e9e4
Simon <simon.schleich@univie.ac.at>
2022-12-05 09:22:02 +0100

    Eureka Spec Conv

--- spectrum_conversion/eureka_to_taurex.py
+++ spectrum_conversion/eureka_to_taurex.py
@@ -1,14 +1,26 @@
-import weakref
+import sys
+import os
 import numpy as np
 
-FILE_NAME = "S6_wasp39b_ap6_bg7_rp^2_Table_Save.txt"
+DATA_ROOT = f"{sys.path[0]}/eureka_spectra"
+FILE_NAME = "ERS1366_wasp39b"
+FILE_EXT = "txt"
 
-raw_array = np.genfromtxt(FILE_NAME, skip_header=10)
-wavelength = raw_array[:, 0]
-rp2 = raw_array[:, 2]
-rp2errup = raw_array[:, 3]
-rp2errdown = raw_array[:, 4]
+def main():
+    """DOC!"""
+    filename = f"{DATA_ROOT}/{FILE_NAME}.{FILE_EXT}"
+    
+    raw_array = np.genfromtxt(filename, skip_header=10)
+    wavelength = raw_array[:, 0]
+    rp2 = raw_array[:, 2]
+    rp2errup = raw_array[:, 3]
+    rp2errdown = raw_array[:, 4]
 
-with open("eureka_nirspec_test.dat", "w") as file:
-    for i in range(len(wavelength)):
-        file.write(f"{wavelength[i]} \t {rp2[i]} \t {rp2errup[i]} \n")
+    with open(f"{FILE_NAME}_spectrum.dat", "w") as file:
+        for i in range(len(wavelength)):
+            file.write(f"{wavelength[i]} \t {rp2[i]} \t {rp2errup[i]} \n")
+
+
+if __name__ == "__main__":
+    main()
+
