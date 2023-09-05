911ab143c8c25050a164e35af5d89c58e1314487
simon-ast <simon.schleich@univie.ac.at>
2023-09-05 15:07:30 +0200

    Restructure plots

--- target_parameters.py
+++ target_parameters.py
@@ -4,6 +4,7 @@ import modules.epa_util as eu
 import modules.epa_custom_plots as ec
 import logging as log
 import sys
+from typing import Union
 import numpy as np
 import pandas as pd
 import copy as cp
@@ -11,8 +12,7 @@ import modules.util as u
 
 # GLOBALS
 DATA_DIR = "data"
-X_AXIS = "pl_orbsmax"
-Y_AXIS = "st_teff"
+QUERY = False
 
 
 def main():
@@ -24,47 +24,28 @@ def main():
         level=log.INFO
     )
 
-    # Reading in the csv-list information
-    file_nc1 = "JWST_cycle1_targets.csv"
-    cycle1 = read_target_csv(DATA_DIR, file_nc1)
-    cycle1["ObsCycle"] = "Cycle 1"
-    file_nc2 = "JWST_cycle2_targets.csv"
-    cycle2 = read_target_csv(DATA_DIR, file_nc2)
-    cycle2["ObsCycle"] = "Cycle 2"
-    csv_combination = join_df([cycle1, cycle2])
+    # Re-query only if necessary
+    if QUERY is True:
+        print("\nFresh query to NASA EPA\n")
 
-    # Querying the NASA EPA with target names from the csv list
-    csv_name = pd.unique(csv_combination["Target Name"])
-    epa_queried = eq.query_nasa_epa(csv_name)
+        # Reading in the csv-list information
+        csv_combination = read_cycles_info()
 
-    # Concatenate csv and epa results (and save df for posterity)
-    constructed = construct_new_df(csv_combination, epa_queried)
-    constructed.to_csv("output/target_parameters/epa_correlation.csv", sep="\t")
+        # Querying the NASA EPA with target names from the csv list
+        constructed = epa_correlation(csv_combination)
+
+    else:
+        print("\n Using existing query results!\n")
+        constructed = pd.read_csv(
+            "output/target_parameters/epa_correlation.csv", sep="\t"
+        )
 
     # Maybe further restrictions?
-    #final = constructed
-    final = constructed.loc[constructed["Type"] == "Transit"]
+    finalised_frame = constructed
+    #finalised_frame = constructed.loc[constructed["Type"] == "Transit"]
 
     # PLOT RESULTS
-    # All targets
-    log.info("ALL TARGETS")
-    eu.plot_parameters(final, x_param=X_AXIS, y_param=Y_AXIS,
-                       savename="all")
-
-    # Sub-neptunes
-    log.info("SUB-NEPTUNES")
-    sn_df = final.loc[final["pl_rade"] <= 4.]
-    eu.plot_parameters(sn_df, x_param=X_AXIS, y_param=Y_AXIS,
-                       savename="subneptunes")
-
-    # HJ and SHJ
-    log.info("SUPER-NEPTUNES")
-    hj_df = final.loc[final["pl_rade"] > 4.]
-    eu.plot_parameters(hj_df, x_param=X_AXIS, y_param=Y_AXIS,
-                       savename="superneptunes")
-
-    # CREATE CUSTOM PLOTS
-    ec.plrad_steff(final, "targets_all")
+    specialised_plot(finalised_frame, "all-types")
 
 
 def read_target_csv(data_dir: str, filename: str) -> pd.DataFrame:
@@ -119,6 +100,88 @@ def construct_new_df(
                 new_df.at[match_idx, column] = epa_subframe.loc[column]
 
     return new_df
+
+
+def read_cycles_info() -> pd.DataFrame:
+    """
+    Hard-coded read-function for observational cycle target information
+    """
+    file_nc1 = "JWST_cycle1_targets.csv"
+    cycle1 = read_target_csv(DATA_DIR, file_nc1)
+    cycle1["ObsCycle"] = "Cycle 1"
+    file_nc2 = "JWST_cycle2_targets.csv"
+    cycle2 = read_target_csv(DATA_DIR, file_nc2)
+    cycle2["ObsCycle"] = "Cycle 2"
+    combination = join_df([cycle1, cycle2])
+
+    return combination
+
+
+def epa_correlation(compiled_data: pd.DataFrame) -> pd.DataFrame:
+    """Substitute target data with query to NASA EPA"""
+    # Querying the NASA EPA with target names from the csv list
+    csv_name = pd.unique(compiled_data["Target Name"])
+    epa_queried = eq.query_nasa_epa(csv_name)
+
+    # Concatenate csv and epa results (and save df for posterity)
+    constructed_frame = construct_new_df(compiled_data, epa_queried)
+    constructed_frame.to_csv(
+        "output/target_parameters/epa_correlation.csv", sep="\t",
+        index=False
+    )
+
+    return constructed_frame
+
+
+def parameter_plot_setup():
+    """Set up specialised plot"""
+    figure, axis = plt.subplots()
+
+    axis.set(
+        xlabel="$R_\\mathrm{p}$ [$R_\\oplus$]", xscale="log",
+        ylabel="$P$ [d]", yscale="log"
+    )
+
+    return figure, axis
+
+
+def parameter_plot_fill(
+        axis: plt.Axes, data_set: pd.DataFrame,
+        colour: Union[str, np.ndarray], opacity=1.0, mec=None
+) -> None:
+    """Fill axis-object with specified data set"""
+    plot = axis.scatter(
+        data_set["pl_rade"], data_set["pl_orbper"],
+        c=colour, alpha=opacity, edgecolor=mec
+    )
+
+    # Insert a colour-bar when necessary
+    if isinstance(colour, np.ndarray):
+        plt.colorbar(plot, label="$T_\\mathrm{eff}$ [K]")
+
+
+def specialised_plot(total_df: pd.DataFrame, savename: str):
+    """Specialised plot wrapper"""
+    # Split into sub-Neptunes and rest
+    not_interest = total_df.loc[total_df["pl_rade"] > 4.0].drop_duplicates(
+        subset=["pl_name"]
+    )
+    of_interest = total_df.loc[total_df["pl_rade"] <= 4.0].drop_duplicates(
+        subset=["pl_name"]
+    )
+
+    # Set up the figure environment
+    fig, ax = parameter_plot_setup()
+
+    # Iteratively fill the figure
+    parameter_plot_fill(ax, not_interest, "grey", opacity=0.4)
+    parameter_plot_fill(
+        ax, of_interest, of_interest["st_teff"].to_numpy(), mec="black"
+    )
+
+    plt.tight_layout()
+    full_save_name = f"target_parameters_{savename}.svg"
+    plt.savefig(f"output/target_parameters/{full_save_name}")
 
 
 if __name__ == "__main__":
